3b38d79ca0d599c66b969ca3e3b580dab344c4d5
Daniel <danielgatis@gmail.com>
2018-06-15 21:55:09 -0300

    5 digits

--- src/main/java/myfaces/captcha/generator/App.java
+++ src/main/java/myfaces/captcha/generator/App.java
@@ -15,47 +15,56 @@ import com.vdurmont.etaprinter.ETAPrinter;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 @Command(name = "java -jar generate")
 public class App implements Runnable {
-	@Parameters(index = "0", description="number of captchas to generate.")
-	private int number = 1;
+    @Parameters(index = "0", description="number of captchas.")
+    private int number = 1;
 
-	@Parameters(index = "1", description="output path.")
-	private String path = ".";
+    @Parameters(index = "1", description="output path.")
+    private String path = ".";
 
-	public void run() {
-		int i = 0;
-		ETAPrinter printer = ETAPrinter.init(number);
-		new File(path).mkdirs();
+    @Option(names = {"--five"},
+            description = "5 digits length.")
+    private boolean isFive = false;
 
-		while (i < number) {
-			MockHttpServletResponse response = new MockHttpServletResponse();
-			String captchaText = CAPTCHATextGenerator.generateRandomText();
+    public void run() {
+        int i = 0;
+        ETAPrinter printer = ETAPrinter.init(number);
+        new File(path).mkdirs();
 
-			if (captchaText.length() < 6) {
-				continue;
-			}
+        while (i < number) {
+            MockHttpServletResponse response = new MockHttpServletResponse();
+            String captchaText = CAPTCHATextGenerator.generateRandomText();
 
-			Color endingColor = ColorGenerator.generateRandomColor(null);
-			Color startingColor = ColorGenerator.generateRandomColor(endingColor);
-			CAPTCHAImageGenerator imageGenerator = new CAPTCHAImageGenerator();
+            if (this.isFive && captchaText.length() != 5) {
+                continue;
+            }
 
-			try {
-				imageGenerator.generateImage(response, captchaText, startingColor, endingColor);
-				FileUtils.writeByteArrayToFile(new File(FilenameUtils.concat(path, captchaText + ".jpg")),
-						response.getContentAsByteArray());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+            if (!this.isFive && captchaText.length() != 6) {
+                continue;
+            }
 
-			i++;
-			printer.update(1);
-		}
-	}
+            Color endingColor = ColorGenerator.generateRandomColor(null);
+            Color startingColor = ColorGenerator.generateRandomColor(endingColor);
+            CAPTCHAImageGenerator imageGenerator = new CAPTCHAImageGenerator();
 
-	public static void main(String[] args) {
-		CommandLine.run(new App(), System.out, args);
-	}
+            try {
+                imageGenerator.generateImage(response, captchaText, startingColor, endingColor);
+                FileUtils.writeByteArrayToFile(new File(FilenameUtils.concat(path, captchaText + ".jpg")),
+                        response.getContentAsByteArray());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            i++;
+            printer.update(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        CommandLine.run(new App(), System.out, args);
+    }
 }
